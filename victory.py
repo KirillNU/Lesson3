920aeb8b521b4149555ffcfae3b23f5a46bbd430
KirillNU <kdbarannik@gmail.com>
2022-11-27 02:02:20 +0300

    quiz 03

--- victory.py
+++ victory.py
@@ -39,51 +39,86 @@ print(result) # [5, 1]
 # Аркадий Стругацкий - 28.07.1925
 # Борис Стругацкий - 15.04.1933
 
-
-
-
-
-# '''
-# j_lond = '1876' #Джек Лондон
-# b_shaw = '1856' #Бернард Шо
-# i_ilf = '1897' #Илья Ильф
-# e_petr = '1902' #Евгений Петров
-# m_jvan = '1934' #Михаил Жванецкий
-# v_akse = '1932' #Василий Аксёнов
-
 import random
 
+max_good = 0
+max_bad = 0
+max_total = 0
+cycle_total = 0
+
+
+lond = {'by': '1876', 'day_dig': '22', 'day_word': 'двадцать второе',
+        'month_dig': '11', 'month_word': 'ноября',
+        'name_genitive': 'Дж. Лондона'}
+
+shaw = {'by': '1856', 'day_dig': '26', 'day_word': 'двадцать шестое',
+        'month_dig': '07', 'month_word': 'июля',
+        'name_genitive': 'Б. Шо'}
+
+ilf = {'by': '1897', 'day_dig': '15', 'day_word': 'пятнадцатое',
+       'month_dig': '10', 'month_word': 'октября',
+       'name_genitive': 'И. Ильфа'}
+
+petr = {'by': '1902', 'day_dig': '13', 'day_word': 'тринадцатое',
+       'month_dig': '12', 'month_word': 'декабря',
+       'name_genitive': 'Е. Петрова'}
+
+jvan = {'by': '1934', 'day_dig': '06', 'day_word': 'шестое',
+       'month_dig': '03', 'month_word': 'марта',
+       'name_genitive': 'М. Жванецкого'}
+
+aks = {'by': '1932', 'day_dig': '20', 'day_word': 'двадцатое',
+       'month_dig': '08', 'month_word': 'августа',
+       'name_genitive': 'В. Аксёнова'}
+
+rol = {'by': '1866', 'day_dig': '29', 'day_word': 'двадцать девятое',
+       'month_dig': '01', 'month_word': 'января',
+       'name_genitive': 'Р. Роллана'}
+
+bul = {'by': '1891', 'day_dig': '15', 'day_word': 'пятнадцатое',
+       'month_dig': '05', 'month_word': 'мая',
+       'name_genitive': 'М. Булгакова'}
+
+astr = {'by': '1925', 'day_dig': '28', 'day_word': 'двадцать восьмое',
+       'month_dig': '07', 'month_word': 'июля',
+       'name_genitive': 'А. Стругацкого'}
+
+bstr = {'by': '1933', 'day_dig': '15', 'day_word': 'пятнадцатое',
+       'month_dig': '04', 'month_word': 'апреля',
+       'name_genitive': 'Б. Стругацкого'}
+
+writers_list = [lond, shaw, ilf, petr, jvan, aks, rol, bul, astr, bstr]
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# while True:
 writers = random.sample(numbers, 5)
 
-print(writers, type(writers))
+ans_good = 0
+ans_bad = 0
+ans_total = 0
 
+for selected in writers:
+    w_name = writers_list[selected-1].get('name_genitive')
+    d_dig = writers_list[selected-1].get('day_dig')
+    d_word = writers_list[selected-1].get('day_word')
+    m_dig = writers_list[selected-1].get('month_dig')
+    m_word = writers_list[selected - 1].get('month_word')
+    b_ye = writers_list[selected-1].get('by')
 
-month_names = {'01': 'января', '02': 'февраля', '03': 'марта',
-               '04': 'апреля', '05': 'мая', '06': 'июня',
-               '07': 'июля', '08': 'августа', '09': 'сентября',
-               '10': 'октября','11': 'ноября','12': 'декабря'
-               }
+    inp_date = input(f'Введите дату рождения {w_name} в формате ДД.ММ.ГГГГ > ')
 
-day_names = {'01': 'первое', '02': 'второе', '03': 'третье', '04': 'четвёртое', '05': 'пятое', '06': 'шестое',
-             '07': 'седьмое', '08': 'восьмое', '09': 'девятое', '10': 'десятое', '11': 'одинадцатое',
-             '12': 'двенадцатое', '13': 'тринадцатое', '14': 'четырнадцатое', '15': 'пятнадцатое',
-             '16': 'шестнадцатое', '17': 'семнадцатое', '18': 'восемнадцатое',
-             '19': 'девятнадцатое', '20': 'двадцатое', '21': 'двадцатьпервое', '22': 'двадцатьвторое',
-             '23': 'двадцатьтретье', '24': 'двадцатьчетвёртое', '25': 'двадцатьпятое', '26': 'двадцатьшестое',
-             '27': 'двадцатьседьмое', '28': 'двадцатьвосьмое', '29': 'двадцатьдевятое',
-             '30': 'тридцатое', '31': 'тридцатьпервое' }
+    if inp_date == f'{d_dig}.{m_dig}.{b_ye}':
+        ans_good += 1
+        ans_total += 1
+    else:
+        ans_bad += 1
+        ans_total += 1
+        print(f'Неверно. День рождения {w_name} {d_word} {m_word} {b_ye} года')
 
-writers_dict = {'уear': '', 'day_dig': '', 'day_word': '', 'month_dig': '', 'month_word': '', 'name_genitive': ''}
+print(ans_good, ans_bad, ans_total, sep='|')
 
-writers_list = []
-
-
-
-# print(month_names)
-# print(day_names)
-# print(writers_dict)
-
-# day_names =
-#
-# writer_dict = {'Year': '1921', "Day_d": , '08', 'Day_w': }
+# while True:
+#     ans_good = 0
+#     ans_bad = 0
+#     ans_total = 0
